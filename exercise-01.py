dd54ed29b0503ea70c1e2e196c78f7199f19ecec
mira <sukmamiranti07@gmail.com>
2023-03-14 14:28:29 +0700

    menambahkan file exercise-01

---
+++ exercise-01.py
@@ -0,0 +1,16 @@
+#exercise 1
+
+print("==========================================================")
+
+nama = ("Miranti Sukma Dewi")
+nim = ("I0322076")
+
+
+kampung_halaman = ("    Saya berasal dari Merauke yang merupakan suatu kota kabupaten yang juga menjadi ibu kota dari provinsi papua selatan. Kondisi geografis Merauke masih relatif alami, sehingga masih banyak ditemui hamparan hijau. Topografi Merauke umumnya datar dan berawa disepanjang pantai dengan kemiringan 0-3% dan ke arah utara mulai dari Distrik Tanah Miring, Jagebob, Elikobel, Muting dan Ulilin keadaan topografinya bergelombang dengan kemiringan 0-8%.")
+harapan_lulusan = ("    Sebagai lulusan teknik industri yang merupakan salah satu jurusan yang mempelajari ilmu yang sangat luas, harapannya setelah lulus dapat mencoba bekerja diberbagai jenis bidang pekerjaan.")
+
+
+print("Nama : ", nama)
+print("NIM  : ", nim)
+print(kampung_halaman)
+print(harapan_lulusan)
